460d426867c05627ef2050866537a311f35a2185
Ramos Esquinas <mario.ramosesquinas@cgi.com>
2022-09-08 17:24:56 +0200

    Cadenas

---
+++ PrimerosPasos/src/ManipulaCadena.java
@@ -0,0 +1,11 @@
+public class ManipulaCadena {
+    public static void main(String[]args) {
+        String nombre="Mario Ramos Esquinas";
+
+        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Mi nombre es " + nombre.length() + " letras");
+        System.out.println("La primera letra de mi " + nombre + " es " + nombre.charAt(0));
+        int utlLetra=nombre.length();
+        System.out.println("La última letra de mi nombre es " + nombre.charAt(utlLetra-1));
+    }
+}

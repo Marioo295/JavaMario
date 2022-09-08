64ff319b8787c07d24ec2cb902ed9114dbeb7e84
Ramos Esquinas <mario.ramosesquinas@cgi.com>
2022-09-08 17:06:29 +0200

    Math avanzado

--- PrimerosPasos/src/CalculosConMath.java
+++ PrimerosPasos/src/CalculosConMath.java
@@ -1,6 +1,11 @@
 public class CalculosConMath {
     public static void main(String[] args){
-        double raiz=Math.sqrt(9);
-        System.out.println(raiz); 
+        //double raiz=Math.sqrt(9);
+        //double num1=5.85;
+        //int resultado=(int) Math.round(num1);
+        double base=5;
+        double exponente=3;
+        double resultado=Math.pow(base, exponente);
+        System.out.println("El resultado de " + base + " elevado a " + exponente + " es " + resultado); 
     }
 }

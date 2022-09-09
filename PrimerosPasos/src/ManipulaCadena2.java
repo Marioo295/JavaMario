9c08813ea57ddb608a736b7ea9edd1216afa5266
Marioo295 <mramosgaliana@gmail.com>
2022-09-09 09:49:55 +0200

    ClaseMathPrueba

---
+++ PrimerosPasos/src/ManipulaCadena2.java
@@ -0,0 +1,11 @@
+public class ManipulaCadena2 {
+    public static void main(String[]args) {
+        String frase = "Hoy es un dia estupendo para aprender Java";
+        String fraseResumen = frase.substring(frase.length()-18, frase.length()) + " y aprender mejor...";
+        System.out.println(fraseResumen);
+
+        String alu1 = "David", alu2= "david";
+        System.out.println(alu1.equals(alu2));
+        System.out.println(alu1.equalsIgnoreCase(alu2));
+    }
+}

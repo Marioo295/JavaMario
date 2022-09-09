2bf92b3775203fa4c4dbc60af5289ba816c6a7b2
Marioo295 <mramosgaliana@gmail.com>
2022-09-09 09:51:36 +0200

    Manipulacion cadenas 2

--- PrimerosPasos/src/ManipulaCadena2.java
+++ PrimerosPasos/src/ManipulaCadena2.java
@@ -4,8 +4,9 @@ public class ManipulaCadena2 {
         String fraseResumen = frase.substring(frase.length()-18, frase.length()) + " y aprender mejor...";
         System.out.println(fraseResumen);
 
-        String alu1 = "David", alu2= "david";
+        String alu1 = "David", alu2= "david", alu3= "David";
         System.out.println(alu1.equals(alu2));
+        System.out.println(alu1.equals(alu3));
         System.out.println(alu1.equalsIgnoreCase(alu2));
     }
 }

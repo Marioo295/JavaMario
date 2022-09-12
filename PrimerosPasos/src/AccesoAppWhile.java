dc8533f5a8626cbc25f5195203596da3e0e9847b
Marioo295 <mramosgaliana@gmail.com>
2022-09-12 10:50:33 +0200

    Primer ejemplo bucle while

---
+++ PrimerosPasos/src/AccesoAppWhile.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class AccesoAppWhile {
+    public static void main(String[] args) {
+        String clave = "mario";
+        String pass = "";
+        while(clave.equals(pass)==false){
+            pass = JOptionPane.showInputDialog("Introduce la contraseña");
+            if(clave.equals(pass)==false){
+                System.out.println("Contraseña incorrecta");
+            }
+        }
+        System.out.println("Contraseña correcta. Acceso permitido");
+    }
+}

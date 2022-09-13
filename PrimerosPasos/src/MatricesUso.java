45d59666d2f61fa5c444f5fc79e6e13082a778ce
Marioo295 <mramosgaliana@gmail.com>
2022-09-13 12:52:00 +0200

    Uso de matrices

---
+++ PrimerosPasos/src/MatricesUso.java
@@ -0,0 +1,16 @@
+public class MatricesUso {
+    public static void main(String[] args) {
+        /*
+        int [] matriz = new int[5];
+        matriz[0] = 10;
+        matriz[1] = 5;
+        matriz[2] = -110;
+        matriz[3] = 70;
+        matriz[4] = 37;
+        */
+        int [] matriz = {10, 5, -110, 70, 37};
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("Valor del índice " + i + " = " + matriz[i]);
+        }
+    }
+}

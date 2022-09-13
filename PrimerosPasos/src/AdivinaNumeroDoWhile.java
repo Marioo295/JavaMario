068f31131b9a4d8a0bc284d88db92f746d25545a
Marioo295 <mramosgaliana@gmail.com>
2022-09-13 11:28:22 +0200

    Adivina numero do while

---
+++ PrimerosPasos/src/AdivinaNumeroDoWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AdivinaNumeroDoWhile {
+    public static void main(String[] args) {
+        int aleatorio = (int)(Math.random()*100);
+        try (Scanner entrada = new Scanner(System.in)) {
+            int numero = 0, intentos = 0;
+            do {
+                intentos++;
+                System.out.print("Introduce un número: ");
+                numero = entrada.nextInt();
+                if (aleatorio<numero) {
+                    System.out.println("Más bajo...");
+                }else if (aleatorio>numero) {
+                    System.out.println("Más alto...");
+                }
+            } while(numero!=aleatorio);
+        System.out.println("Correcto. Lo has conseguido en " + intentos + " intentos.");
+        }
+    }
+}

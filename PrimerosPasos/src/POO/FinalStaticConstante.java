0a5ec2b2329c0552d517f13f5e5090f8db1df6b8
Marioo295 <mramosgaliana@gmail.com>
2022-09-14 12:17:10 +0200

    Final static variables

--- PrimerosPasos/src/POO/FinalStaticConstante.java
+++ PrimerosPasos/src/POO/FinalStaticConstante.java
@@ -1,26 +1,34 @@
 package POO;
 
-public class FinalConstante {
+public class FinalStaticConstante {
     public static void main(String[] args) {
         Empleados trabajador1 = new Empleados("Paco");
         Empleados trabajador2 = new Empleados("Ana");
+        Empleados trabajador3 = new Empleados("Antonio");
+        Empleados trabajador4 = new Empleados("Fernando");
         trabajador1.setSeccion("RRHH");
         System.out.println(trabajador1.getDatos());
         System.out.println(trabajador2.getDatos());
+        System.out.println(trabajador3.getDatos());
+        System.out.println(trabajador4.getDatos());
     }
 }
 
 class Empleados {
     private final String NOMBRE;
     private String seccion;
+    private int id;
+    private static int idSiguiente = 1;
 
     public Empleados(String nom) {
         NOMBRE = nom;
         seccion = "Administracion";
+        id=idSiguiente;
+        idSiguiente++;
     }
 
     public String getDatos(){
-        return "El nombre es: " +NOMBRE+ " y la sección es " +seccion+ ".";
+        return "El nombre es: " +NOMBRE+ " y la sección es " +seccion+ " y el ID = " +id+ ".";
     }
 
     public void setSeccion(String seccion) {
